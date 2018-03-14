3085da6205dd060ca902fdd84f0816784aac5110
becca <chowr@purdue.edu>
2018-03-13 23:27:15 -0400

    create a checkError function for Percolation.java

--- UnionFind/src/Percolation.java
+++ UnionFind/src/Percolation.java
@@ -3,12 +3,13 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     //fields
-    int sizeN;
-    int openSites;
-    int [][] n_n_grid;
-    WeightedQuickUnionUF unionFindArray;
-    int top;
-    int bottom;
+    private final int sizeN;
+    private int openSites;
+    private int [][] n_n_grid;
+    private final WeightedQuickUnionUF unionFindArray;
+    private final WeightedQuickUnionUF unionFindFull;
+    private final int top;
+    private final int bottom;
 
     //default constructor
     public Percolation (int n) {
@@ -17,6 +18,7 @@ public class Percolation {
         }
         this.n_n_grid = new int [n][n];
         this.unionFindArray = new WeightedQuickUnionUF(n*n + 2);
+        this.unionFindFull = new WeightedQuickUnionUF(n*n+1);
         this.sizeN = n;
         this.openSites = 0;
         this.top = 0;
@@ -24,6 +26,7 @@ public class Percolation {
     }
 
     public void open (int row, int col) {
+        checkError(row, col);
         if (!isOpen(row, col)) {
             //open the site
             this.n_n_grid[row - 1][col - 1] = 1;
@@ -37,8 +40,9 @@ public class Percolation {
             //connect to virtual sites if opened site is the first/last row
             if(row == 1 && !unionFindArray.connected(ufArrayIndex(row, col), this.top)) {
                 unionFindArray.union(ufArrayIndex(row, col), this.top);
+                unionFindFull.union(ufArrayIndex(row, col), this.top);
             }
-            else if(row == this.sizeN && !unionFindArray.connected(ufArrayIndex(row, col), this.bottom)) {
+            if(row == this.sizeN && !unionFindArray.connected(ufArrayIndex(row, col), this.bottom)) {
                 unionFindArray.union(ufArrayIndex(row, col), this.bottom);
             }
 
@@ -47,6 +51,7 @@ public class Percolation {
     }
 
     public boolean isOpen (int row, int col) {
+        checkError(row, col);
         if((row - 1) < 0 || (row - 1 >= this.sizeN) || (col - 1) < 0 || (col - 1) >= this.sizeN) {
             //check boundary of the site for checkAdjAndUnion func
             return false;
@@ -61,14 +66,16 @@ public class Percolation {
 
     public boolean isFull (int row, int col) {
         //if it is connected to the top
-        return unionFindArray.connected(ufArrayIndex(row, col), this.top);
+        //unionFindFull don't connect to the bott
+        checkError(row, col);
+        return unionFindFull.connected(ufArrayIndex(row, col), this.top);
     }
 
     public int numberOfOpenSites() {
         return this.openSites;
     }
 
-    public boolean percoltaes() {
+    public boolean percolates() {
         return unionFindArray.connected(this.top, this.bottom);
     }
 
@@ -82,19 +89,17 @@ public class Percolation {
         if(isOpen(row, col) && !this.unionFindArray.connected(ufArrayIndex(oriRow, oriCol), ufArrayIndex(row, col))) {
                 this.unionFindArray.union(ufArrayIndex(oriRow, oriCol), ufArrayIndex(row, col));
         }
-    }
-
-    //Methods for testing purpose
-    private void print_n_n_grid_xy() {
-        for(int i = 0; i < this.sizeN; i++) {
-            for(int j = 0; j < this.sizeN; j++) {
-                System.out.print(i+1);
-                System.out.print((j+1) + " ");
-            }
-            System.out.println();
+        if(isOpen(row, col) && !this.unionFindFull.connected(ufArrayIndex(oriRow, oriCol), ufArrayIndex(row, col))) {
+            this.unionFindFull.union(ufArrayIndex(oriRow, oriCol), ufArrayIndex(row, col));
         }
     }
 
+    private void checkError(int row, int col) {
+        if(row <= 0 || row > this.sizeN || col <= 0 || col > this.sizeN) {
+            throw new IllegalArgumentException("The row and column value should be integer in [1, n]");
+        }
+    }
+    //Methods for testing purpose
     private void print_n_n_grid_val() {
         for(int i = 0; i < this.sizeN; i++) {
             for(int j = 0; j < this.sizeN; j++) {
@@ -113,8 +118,8 @@ public class Percolation {
         return coord;
     }
 
-    public int runTillPercolates(Percolation n_n_percolation_test) {
-        while(!n_n_percolation_test.percoltaes()) {
+    private int runTillPercolates(Percolation n_n_percolation_test) {
+        while(!n_n_percolation_test.percolates()) {
             int[] xy  = n_n_percolation_test.test_xy();
             n_n_percolation_test.open(xy[0], xy[1]);
         }
@@ -126,11 +131,15 @@ public class Percolation {
     public static void main (String[] args)
     {
         //create a 6 by 6 grid
-        Percolation n_n_percolation_test = new Percolation(15);
+        Percolation n_n_percolation_test = new Percolation(1);
         //n_n_percolation_test.print_n_n_grid_xy();
         //n_n_percolation_test.print_n_n_grid_val();
 
-        System.out.print(n_n_percolation_test.runTillPercolates(n_n_percolation_test));
-
+        //System.out.println(n_n_percolation_test.runTillPercolates(n_n_percolation_test));
+        //n_n_percolation_test.print_n_n_grid_val();
+        //System.out.print(n_n_percolation_test.isFull(15,6));
+        n_n_percolation_test.open(1,1);
+        n_n_percolation_test.print_n_n_grid_val();
+        System.out.println(n_n_percolation_test.percolates());
     }
 }
--- UnionFind/src/PercolationStats.java
+++ UnionFind/src/PercolationStats.java
@@ -1,22 +1,29 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import java.lang.Math;
+import java.lang.Integer;
+
 
 public class PercolationStats {
-    double[] thresholds;
-    int trials;
-    int gridSize;
-    double meanVal;
-    double stddevVal;
+    private final double[] thresholds;
+    private final int trials;
+    private double meanVal;
+    private double stddevVal;
+    private final int sizeN;
+    private static double CONFIDENCE_95 = 1.96;
 
     public PercolationStats(int n, int trials) {
+        if (trials <= 0) {
+            throw new IllegalArgumentException("trials should be greater than 0!");
+        }
+        this.sizeN = n;
         this.trials = trials;
-        this.gridSize = n*n;
         this.thresholds = new double[trials];
         this.meanVal = 0;
         this.stddevVal = 0;
         for(int i = 0; i < trials; i++) {
             Percolation newTest = new Percolation(n);
-            this.thresholds[i] = newTest.runTillPercolates(newTest) / (double) this.gridSize;
+            this.thresholds[i] = this.runTillPercolates(newTest) / (double) (n*n);
         }
     }
 
@@ -31,18 +38,37 @@ public class PercolationStats {
     }
 
     public double confidenceLo() {
-        return this.meanVal - 1.96 * this.stddevVal / Math.sqrt((double) this.trials);
+        return this.meanVal - CONFIDENCE_95 * this.stddevVal / Math.sqrt((double) this.trials);
     }
 
     public double confidenceHi() {
-        return this.meanVal + 1.96 * this.stddevVal / Math.sqrt((double) this.trials);
+        return this.meanVal + CONFIDENCE_95 * this.stddevVal / Math.sqrt((double) this.trials);
+    }
+
+    private int[] test_xy() {
+        int[] coord = new int[2];
+        coord[0] = StdRandom.uniform(this.sizeN) + 1;
+        coord[1] = StdRandom.uniform(this.sizeN) + 1;
+        //System.out.print(coord[0] + " ");
+        //System.out.println(coord[1]);
+        return coord;
+    }
+
+    private int runTillPercolates(Percolation n_n_percolation_test) {
+        while(!n_n_percolation_test.percolates()) {
+            int[] xy  = this.test_xy();
+            n_n_percolation_test.open(xy[0], xy[1]);
+        }
+
+        return n_n_percolation_test.numberOfOpenSites();
     }
 
     public static void main(String[] args) {
-        PercolationStats test = new PercolationStats(200, 100);
+        PercolationStats test = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println(test.mean());
         System.out.println(test.stddev());
         System.out.println(test.confidenceLo());
         System.out.println(test.confidenceHi());
+
     }
 }
