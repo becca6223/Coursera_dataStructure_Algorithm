8a8485e944f0a540e13f47402835afc38c1f57a5
becca <chowr@purdue.edu>
2018-08-24 15:14:39 -0400

    Delete original Deque.java. Deque can do addFirst, addLast, and double array size

--- StacksAndQueues/src/Deque.java
+++ StacksAndQueues/src/Deque.java
@@ -1,66 +1,103 @@
+import java.lang.IllegalArgumentException;
+import java.util.NoSuchElementException;
+import java.lang.UnsupportedOperationException;
+import java.lang.*;
 import java.util.Iterator;
 
-public class Deque <Item> implements Iterable <Item> {
-    //inner class
-    private class Node {
-        Item item;
-        Node next;
-    }
+public class Deque <Item> implements Iterable<Item> {
 
-    //member variables
-    private Node head;
-    private Node secondLast;
-    private Node last;
-    private int n;
+    private Item[] array = (Item[]) new Object[0];
+    private int num_elements = 0;
+    private int array_size = 0;
+    private int hpt = -1;
+    private int tpt = -1;
 
     public Deque() {
-        //Constructor: create an empty Deque
-        this.head = null;
-        this.secondLast = null;
-        this.n = 0; //will help size function
-    }
-
-    public boolean isEmpty() {
-        return this.n == 0;
-    }
-
-    public int size() {
-        return this.n;
-    }
-
-    public void addFirst(Item item) {
-        Node oldHead = this.head;
-
-        this.head = new Node();
-        this.head.next = oldHead;
-        this.head.item = item;
-
-        this.n += 1;
-        update2ndLast();
-    }
-
-    public void addLast(Item item) {
 
     }
-    public Iterator<Item> iterator() {
-        //What is iterator?
-        return iterator();
+
+    public boolean isEmpty () {
+        return this.num_elements == 0;
     }
 
-    //Private methods
-    private void update2ndLast() {
-        if(this.n == 2) {
-            this.secondLast = this.head;
+    public int size () {
+        return this.num_elements;
+    }
+
+    public void addFirst (Item item) {
+        if (isFull()) {
+            doubleSize();
         }
-        else if(this.n > 2) {
-            this.secondLast = this.secondLast.next;
+        if (isEmpty()) {
+            this.hpt = 0;
+            this.tpt = 0;
         }
         else {
-            this.secondLast = null;
+            this.hpt = (this.hpt + 1) % array_size;
         }
+
+        array[this.hpt] = item;
+        this.num_elements++;
     }
 
-   /* public static void main(String[] args) throws Exception {
+    public void addLast (Item item) {
+        if (isFull()) {
+            doubleSize();
+        }
+        if (isEmpty()) {
+            this.hpt = 0;
+            this.tpt = 0;
+        }
+        else {
+            this.tpt = (this.tpt + this.array_size - 1) % this.array_size;
+        }
 
-    }*/
+        array[this.tpt] = item;
+        this.num_elements++;
+    }
+
+    public Iterator<Item> iterator () {
+        return new DequeueIterator();
+    }
+
+    private class DequeueIterator implements Iterator<Item> {
+        private int cur_index = 0;
+
+        public boolean hasNext() {return true;}
+        public Item next() {
+            return array[0];
+        }
+
+    }
+
+    //HELPER FUNCTIONS
+    private boolean isFull () {
+        return this.array_size == this.num_elements;
+    }
+
+    private void doubleSize() {
+        int new_size = this.array_size == 0 ? 1 : 2 * this.array_size; //double array size
+        Item[] copy = (Item[]) new Object[new_size];
+
+        int head_length = this.hpt + 1; //number of elements to be copied from 0 to hpt
+        System.arraycopy(this.array, 0, copy, 0, head_length);
+        if (head_length != this.array_size) {
+            //copy if the previous copy didn't copy the whole array
+            int tail_length = this.array_size - head_length;
+            System.arraycopy(this.array, this.tpt, copy, new_size - tail_length, tail_length);
+
+        }
+
+        //Final Update
+        this.array_size = new_size;
+        this.array = copy;
+
+    }
+
+    public static void main(String[] args) {
+        Deque <Integer> deque = new Deque<Integer>();
+        deque.addFirst(1);
+        deque.addLast(2);
+        System.out.print("hi");
+    }
 }
